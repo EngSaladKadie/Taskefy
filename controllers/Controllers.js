57bd7ed68ea15499ff5f95e24e5ca132b9c4e010
zalad <zaladyusuf@gmail.com>
2024-10-29 20:30:20 +0300

    secondCommit

--- controllers/Controllers.js
+++ controllers/Controllers.js
@@ -1,9 +1,43 @@
+const {incomForm} = require("formidable");
+const { readTask } = require("../utils/fileHanddler")
+
+
 exports.GetTask = (req, res) => {
+    const tasks= readTask()
+    res.writeHead(200, "OK", {"Content-type":"Application/json"});
+    res.send(JSON.stringify(tasks))
 
 }
 exports.CreatTask = (req, res) => {
+   const form = new incomForm();
+   form.parse(req, (err, fields, files) => {
+       if (err) {
+           res.writeHead(500, "Not Found", {"Content-type":"Application/json"});
+           res.end(JSON.stringify({
+               message:"Internal Server Error"
+           }))
+           return;
+       }
+     const tasks= readTask();
+     const newTask={
+        id:  Date.now(),
+        title: fields.title,
+        description: fields.description,
+        status: fields?.status || 'pendding'
+        
+     }
+   })
+   
+        
+    
+
     
 }
+
+
+
+
+
 exports.UpdateTask = (req, res) => {
     
 }
--- utils/fileHanddler.js
+++ utils/fileHanddler.js
@@ -2,13 +2,15 @@ const fs= require('fs');
 const path= require('path');
 
 const filePath= '.data/tasks.json';
-const writeTask=(task)=>{
+exports.writeTask=(task)=>{
     fs.writeFileSync(filePath,JSON.stringify(task));
     
 }
-const readTask=()=>{
+exports.readTask=()=>{
     if (!fs.existsSync(filePath)) {
         writeTask([]);
         
     }
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
 }
